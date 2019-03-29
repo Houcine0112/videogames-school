e04ba924f2f7aee0446959b8f6e980636d44867d
lahoucine.elaidous.etu <lahoucine.elaidous.etu@univ-lille.fr>
2019-03-29 12:28:22 +0100

    front-end : beautify code; debugging ordering

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,5 +1,4 @@
 var dir = window.location.pathname;
-
 var headers = [
     "id",
     "Name",
@@ -7,11 +6,11 @@ var headers = [
     "Year_of_Release",
     "Genre",
     "Publisher",
-    "NA_Sales",
-    "EU_Sales",
+    //"NA_Sales",
+    //"EU_Sales",
     //"JP_Sales",
     //"Other_Sales",
-    "Global_Sales",
+    "Global_Sales"
     //"Critic_Score",
     //"Critic_Count",
     //"User_Score",
@@ -19,69 +18,49 @@ var headers = [
     //"Developer",
     //"Rating"
 ];
-
-
-// default params
+// default GET params
 var params = {
     page: 1,
     page_rows_nb: 10,
     sortBy: "id",
     reverse: false
 };
-
-// change params case existe in the link
 var params_url_format = window.location.search.substring(1, window.location.search.length);
 var nb_lines = 16719;
 
-function update_url() {
-    window.history.pushState('', 'TableauxDnamique', dir + "?" + $.param(params));
-}
-
-function init_params(){
+// case the URL contains GET params
+function init_params() {
     if (params_url_format !== "") {
+        // build JSON from url params
         params = JSON
             .parse('{"' + decodeURI(params_url_format
                 .replace(/&/g, "\",\"")              // replace '&' with ','
                 .replace(/=/g, "\":\"")) + '"}'      // replace '=' with ':'
             );
+        // cast strings to the right type
         params.page_rows_nb = parseInt(params.page_rows_nb);
-        params.page         = parseInt(params.page);
-        params.reverse      = (params.reverse == 'true');
+        params.page = parseInt(params.page);
+        params.reverse = (params.reverse === 'true');
     }
+    // else we will keep the initial params (look global variables)
+}
+
+// case we change something in the params
+function update_url() {
+    window.history.pushState('', 'TableauxDnamique', dir + "?" + $.param(params));
 }
 
 function sort_table() {
-    params.page = 1;
-    insert_table_headers();
+    params.page = 1; // every new sort begins in the first page
     update_url();
+    insert_table_headers();
     insert_table_body();
 }
 
-function insert_table_body() {
-    $.ajax("/api/videoGames/all?sortBy=" + params.sortBy + "&reverse=" +
-        params.reverse + "&page=" + params.page + "&page_rows_nb=" + params.page_rows_nb)
-        .done(function (arg) {
-            $("#mainbody").empty();
-            $("#page").empty();
-            $("#page").append("<h1 class=\"display-4 \" > Page : " + params.page + "</h1>");
-            $.each(arg, function (index, row) {
-                $("#mainbody").append("<tr id=row-" + row.id + ">");
-                $.each(headers, function (index, field) {
-                    if (field === "id") $("#mainbody #row-" + row.id).append("<th scope='col'>" + row[field] + "</th>");
-                    else $("#mainbody #row-" + row.id).append("<td>" + row[field] + "</td>");
-                });
-
-            });
-        })
-        .fail(function () {
-            alert("error");
-        });
-}
-
 function insert_table_headers() {
-    $("#mainheader").empty();
+    $("#mainTableHeader").empty();
     $.each(headers, function (index, data) {
-        $("#mainheader")
+        $("#mainTableHeader")
             .append("<th scope='col' id='col-" + data + "' style='cursor: pointer;'>" + data + "</th>");
         $("#col-" + data)
             .click(function () {
@@ -92,6 +71,38 @@ function insert_table_headers() {
             });
     });
     $("#col-" + params.sortBy).append(params.reverse ? "▼" : "▲");
+}
+
+function insert_table_body() {
+    $.ajax(
+        "/api/videoGames/all" +
+        "?sortBy=" + params.sortBy +
+        "&reverse=" + params.reverse +
+        "&page=" + params.page +
+        "&page_rows_nb=" + params.page_rows_nb
+    )
+        .done(function (arg) {
+            $("#page")
+                .empty()
+                .append("<h1 class=\"display-4 \" > Page : " + params.page + "</h1>");
+            $("#mainTableBody").empty();
+            $.each(arg, function (index, row) {
+                $("#mainTableBody").append("<tr id=row-" + row.id + ">");
+                $.each(headers, function (index, field) {
+                    // the 'id' header is styled differently
+                    if (field === "id")
+                        $("#mainTableBody #row-" + row.id)
+                            .append("<th scope='col'>" + row[field] + "</th>");
+                    else
+                        $("#mainTableBody #row-" + row.id)
+                            .append("<td>" + row[field] + "</td>");
+                });
+
+            });
+        })
+        .fail(function () {
+            alert("error");
+        });
 }
 
 
