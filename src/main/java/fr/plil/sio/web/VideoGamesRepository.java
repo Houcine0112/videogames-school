84457aba4eb8c1becc268531d1f4064f3dc12b9f
lahoucine.elaidous.etu <lahoucine.elaidous.etu@univ-lille.fr>
2019-03-29 10:48:46 +0100

    back-end : code refactoring and beautifying

--- src/main/java/fr/plil/sio/web/VideoGamesRepository.java
+++ src/main/java/fr/plil/sio/web/VideoGamesRepository.java
@@ -26,78 +26,62 @@ public class VideoGamesRepository {
     
     public List<VideoGame> getSortedBy(String sortBy, boolean reverse){
         List<VideoGame> list_sorted = new ArrayList<>(videoGames);
-        // cratorsomparators
-        Comparator<VideoGame> compareById = (VideoGame v1, VideoGame v2) -> v1.getId().compareTo(v2.getId());
-        Comparator<VideoGame> compareByName = (VideoGame v1, VideoGame v2) -> v1.getName().toUpperCase().compareTo(v2.getName().toUpperCase());
-        Comparator<VideoGame> compareByPlatform = (VideoGame v1, VideoGame v2) -> v1.getPlatform().toUpperCase().compareTo(v2.getPlatform().toUpperCase());
-        Comparator<VideoGame> compareByYear_of_Release = (VideoGame v1, VideoGame v2) -> v1.getYear_of_Release().compareTo(v2.getYear_of_Release());
-        Comparator<VideoGame> compareByGenre = (VideoGame v1, VideoGame v2) -> v1.getGenre().toUpperCase().compareTo(v2.getGenre().toUpperCase());
-        Comparator<VideoGame> compareByPublisher = (VideoGame v1, VideoGame v2) -> v1.getPublisher().toUpperCase().compareTo(v2.getPublisher().toUpperCase());
-        Comparator<VideoGame> compareByNA_Sales = (VideoGame v1, VideoGame v2) -> v1.getNA_Sales().compareTo(v2.getNA_Sales());
-        Comparator<VideoGame> compareByEU_Sales = (VideoGame v1, VideoGame v2) -> v1.getEU_Sales().compareTo(v2.getEU_Sales());
-        Comparator<VideoGame> compareByJP_Sales = (VideoGame v1, VideoGame v2) -> v1.getJP_Sales().compareTo(v2.getJP_Sales());
-        Comparator<VideoGame> compareByOther_Sales = (VideoGame v1, VideoGame v2) -> v1.getOther_Sales().compareTo(v2.getOther_Sales());
-        Comparator<VideoGame> compareByGlobal_Sales = (VideoGame v1, VideoGame v2) -> v1.getGlobal_Sales().compareTo(v2.getGlobal_Sales());
-        Comparator<VideoGame> compareByCritic_Score = (VideoGame v1, VideoGame v2) -> v1.getCritic_Score().compareTo(v2.getCritic_Score());
-        Comparator<VideoGame> compareByCritic_Count = (VideoGame v1, VideoGame v2) -> v1.getCritic_Count().compareTo(v2.getCritic_Count());
-        Comparator<VideoGame> compareByUser_Score = (VideoGame v1, VideoGame v2) -> v1.getUser_Score().compareTo(v2.getUser_Score());
-        Comparator<VideoGame> compareByUser_Count = (VideoGame v1, VideoGame v2) -> v1.getUser_Count().compareTo(v2.getUser_Count());
-        Comparator<VideoGame> compareByDeveloper = (VideoGame v1, VideoGame v2) -> v1.getDeveloper().toUpperCase().compareTo(v2.getDeveloper().toUpperCase());
-        Comparator<VideoGame> compareByRating = (VideoGame v1, VideoGame v2) -> v1.getRating().toUpperCase().compareTo(v2.getRating().toUpperCase());
+        //
+        Comparator<VideoGame> comparator;
+
         switch(sortBy) {
-            case "id":
-                list_sorted.sort(!reverse ? compareById : compareById.reversed());
-                break;
             case "Name":
-                list_sorted.sort(!reverse ? compareByName : compareByName.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getName().toUpperCase().compareTo(v2.getName().toUpperCase());
               break;
             case "Platform":
-                list_sorted.sort(!reverse ? compareByPlatform : compareByPlatform.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getPlatform().toUpperCase().compareTo(v2.getPlatform().toUpperCase());
               break;
             case "Year_of_Release":
-                list_sorted.sort(!reverse ? compareByYear_of_Release : compareByYear_of_Release.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getYear_of_Release().compareTo(v2.getYear_of_Release());
               break;
             case "Genre":
-                list_sorted.sort(!reverse ? compareByGenre : compareByGenre.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getGenre().toUpperCase().compareTo(v2.getGenre().toUpperCase());
               break;
             case "Publisher":
-                list_sorted.sort(!reverse ? compareByPublisher : compareByPublisher.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getPublisher().toUpperCase().compareTo(v2.getPublisher().toUpperCase());
               break;
             case "NA_Sales":
-                list_sorted.sort(!reverse ? compareByNA_Sales : compareByNA_Sales.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getNA_Sales().compareTo(v2.getNA_Sales());
               break;
             case "EU_Sales":
-                list_sorted.sort(!reverse ? compareByEU_Sales : compareByEU_Sales.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getEU_Sales().compareTo(v2.getEU_Sales());
               break;
             case "JP_Sales":
-                list_sorted.sort(!reverse ? compareByJP_Sales : compareByJP_Sales.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getJP_Sales().compareTo(v2.getJP_Sales());
               break;
             case "Other_Sales":
-                list_sorted.sort(!reverse ? compareByOther_Sales : compareByOther_Sales.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getOther_Sales().compareTo(v2.getOther_Sales());
               break;
             case "Global_Sales":
-                list_sorted.sort(!reverse ? compareByGlobal_Sales : compareByGlobal_Sales.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getGlobal_Sales().compareTo(v2.getGlobal_Sales());
               break;
             case "Critic_Score":
-                list_sorted.sort(!reverse ? compareByCritic_Score : compareByCritic_Score.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getCritic_Score().compareTo(v2.getCritic_Score());
               break;
             case "Critic_Count":
-                list_sorted.sort(!reverse ? compareByCritic_Count : compareByCritic_Count.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getCritic_Count().compareTo(v2.getCritic_Count());
               break;
             case "User_Score":
-                list_sorted.sort(!reverse ? compareByUser_Score: compareByUser_Score.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getUser_Score().compareTo(v2.getUser_Score());
               break;
             case "User_Count":
-                list_sorted.sort(!reverse ? compareByUser_Count : compareByUser_Count.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getUser_Count().compareTo(v2.getUser_Count());
               break;
             case "Developer":
-                list_sorted.sort(!reverse ? compareByDeveloper : compareByDeveloper.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getDeveloper().toUpperCase().compareTo(v2.getDeveloper().toUpperCase());
               break;
             case "Rating":
-                list_sorted.sort(!reverse ? compareByRating : compareByRating.reversed());
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getRating().toUpperCase().compareTo(v2.getRating().toUpperCase());
               break;
             default:
+                comparator = (VideoGame v1, VideoGame v2) -> v1.getId().compareTo(v2.getId());
         }
+        list_sorted.sort(!reverse ? comparator : comparator.reversed());
         return list_sorted;
     }
 }
